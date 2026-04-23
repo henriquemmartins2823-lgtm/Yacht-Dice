531c154f354d3a1fb8f1efb1c920ebd548d82cae
Leonardo Santana <leonardops6@al.insper.edu.br>
2026-04-22 21:15:36 -0300

    Função remover_dado

--- funcoes.py
+++ funcoes.py
@@ -15,5 +15,20 @@ def guardar_dado(rolado,estoque,guardar):
     listafinal.append(listanovarolado)
     listafinal.append(estoque)
     return listafinal
+def remover_dado(rolado,estoque,remover):
+    listanovarolado1=rolado
+    estoquenovo=[]
+    for i in range(len(estoque)):
+        if i==remover:
+            listanovarolado1.append(estoque[i])
+        else:
+            estoquenovo.append(estoque[i])
+    listafinal=[]
+    listafinal.append(listanovarolado1)
+    listafinal.append(estoquenovo)
+    return listafinal
+
+
+
 
 
